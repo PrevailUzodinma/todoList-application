3ece8a717db258531696f631bd068de01cc16996
Prevail Uzodinma <prevailuzodinma@gmail.com>
2024-02-25 14:07:00 +0100

    I added the offline version of font awesome, I also added the functionality to make the plus sign toggle on and off

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,6 +1,8 @@
 let input = document.querySelector("input[type='text']");
 let ul = document.querySelector("ul");
+let plusIcon = document.querySelector(".fa-plus");
 
+// function to make my listener activated on newly written todos from the input tag
 function attachListeners() {
   let lis = document.querySelectorAll("li");
   let spans = document.querySelectorAll("span");
@@ -36,12 +38,68 @@ input.addEventListener("keypress", function (event) {
     let newLi = document.createElement("li");
 
     // Set the text content of the li element
-    newLi.innerHTML = "<span>" + "X" + "</span>" + " " + todoText;
+    newLi.innerHTML =
+      "<span>" +
+      '<i class="fa-regular fa-trash-can"></i>' +
+      "</span>" +
+      " " +
+      todoText;
 
     // Append the new li element to the ul
     ul.appendChild(newLi);
 
     // Attaching event listeners to  the new todo
     attachListeners();
+  }
+});
+
+// function that will fade out my input tag when I click the plusIcon
+function fadeOut(element) {
+  var opacity = 1;
+  var interval = 50;
+  var duration = 50;
+
+  var fadeOutInterval = setInterval(function () {
+    if (opacity > 0) {
+      opacity -= interval / duration;
+      element.style.opacity = opacity;
+    } else {
+      clearInterval(fadeOutInterval);
+      element.style.display = "none";
+    }
+  }, interval);
+}
+
+// function that will cause my input tag to re-emerge when I hit the input tag.
+function fadeIn(element) {
+  var opacity = 0;
+  var interval = 50;
+  var duration = 50;
+
+  element.style.opacity = opacity;
+  element.style.display = "block";
+
+  var fadeInInterval = setInterval(function () {
+    if (opacity < 1) {
+      opacity += interval / duration;
+      element.style.opacity = opacity;
+    } else {
+      clearInterval(fadeInInterval);
+    }
+  }, interval);
+}
+
+// Adding the listener onCalling the respective functions on my input tag.
+
+plusIcon.addEventListener("click", function () {
+  if (input.style.display === "none") {
+    // If the input is currently hidden, fade it in
+    input.style.display = "block";
+    fadeIn(input);
+  } else {
+    // If the input is currently visible, fade it out
+    fadeOut(input, function () {
+      input.style.display = "none";
+    });
   }
 });
